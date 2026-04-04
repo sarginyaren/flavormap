372e89f7da511dafd5843e586dca787b3683f470
Esin Sarıaltın <eesarialtin7@gmail.com>
2026-04-04 11:00:16 +0300

    Merge conflict resolved: urls, requirements, forms

--- flavor/urls.py
+++ flavor/urls.py
@@ -1,5 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+from restaurants import views as restaurant_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/register/', restaurant_views.register, name='register'),
+    path('', include('restaurants.urls')),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
